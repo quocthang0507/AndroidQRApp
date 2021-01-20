1bbd6ea05ae6b428441024faae957996b89f0af6
La Quốc Thắng <quocthang0507@gmail.com>
2021-01-20 09:49:12 +0700

    Update GUI

--- QR App/MainActivity.cs
+++ QR App/MainActivity.cs
@@ -8,43 +8,42 @@ using Android.Widget;
 
 namespace QR_App
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
-    {
-        TextView textMessage;
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@drawable/qr_code")]
+	public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+	{
+		TextView textMessage;
 
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			SetContentView(Resource.Layout.activity_main);
 
-            textMessage = FindViewById<TextView>(Resource.Id.message);
-            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
-            navigation.SetOnNavigationItemSelectedListener(this);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			textMessage = FindViewById<TextView>(Resource.Id.message);
+			BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+			navigation.SetOnNavigationItemSelectedListener(this);
+		}
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-        public bool OnNavigationItemSelected(IMenuItem item)
-        {
-            switch (item.ItemId)
-            {
-                case Resource.Id.navigation_home:
-                    textMessage.SetText(Resource.String.title_home);
-                    return true;
-                case Resource.Id.navigation_dashboard:
-                    textMessage.SetText(Resource.String.title_dashboard);
-                    return true;
-                case Resource.Id.navigation_notifications:
-                    textMessage.SetText(Resource.String.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    }
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		{
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+
+		public bool OnNavigationItemSelected(IMenuItem item)
+		{
+			switch (item.ItemId)
+			{
+				case Resource.Id.nav_scanner:
+					textMessage.SetText(Resource.String.title_scanner);
+					return true;
+				case Resource.Id.nav_generator:
+					textMessage.SetText(Resource.String.title_generator);
+					return true;
+			}
+			return false;
+		}
+	}
 }
 
