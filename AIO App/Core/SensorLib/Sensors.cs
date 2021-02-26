c3219b914a69ecce064d0a9592338482b64c6583
La Quốc Thắng <quocthang0507@gmail.com>
2021-02-26 20:49:22 +0700

    Move some files into folder

---
+++ AIO App/Core/SensorLib/Sensors.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Hardware;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIOApp.SensorLib
+{
+	public class Sensors
+	{
+		public SensorManager sensorManager;
+
+		private static Sensors singleton;
+
+		public Sensors()
+		{
+			sensorManager = (SensorManager)Application.Context.GetSystemService(Context.SensorService);
+		}
+
+		public static Sensors Instance
+		{
+			get
+			{
+				if (singleton == null)
+					singleton = new Sensors();
+				return singleton;
+			}
+		}
+	}
+}
